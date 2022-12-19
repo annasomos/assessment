c71aa51da9c9eb8b0a68b87106b609b257a9ba45
annasomos <annasomoss@gmail.com>
2022-12-19 15:52:08 +0100

    Refactor: Autoformatting

--- js-numerals/conversion/src/__tests__/App.spec.js
+++ js-numerals/conversion/src/__tests__/App.spec.js
@@ -1,301 +1,326 @@
 import App from "../App";
-import {render, screen, cleanup, userEvent, fireEvent} from '@testing-library/react';
+import { render, screen, cleanup, fireEvent } from "@testing-library/react";
 
-beforeEach(()=> {
+beforeEach(() => {
   cleanup();
-})
+});
 
-afterEach(()=> {
+afterEach(() => {
   cleanup();
-})
+});
 
-test('User submits 7', () => {
-  render(<App/>)
+test("User submits 7", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '7'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "7" } });
   fireEvent.click(button);
   const text = screen.getAllByText(/seven/);
   expect(text.length).toBe(1);
-})
+});
 
-test('User submits -7', () => {
-  render(<App/>)
+test("User submits -7", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-7'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-7" } });
   fireEvent.click(button);
   const text = screen.getAllByText(/negative seven/);
   expect(text.length).toBe(1);
-})
+});
 
-test('User submits 42', () => {
-  render(<App/>)
+test("User submits 42", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '42'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "42" } });
   fireEvent.click(button);
   const text = screen.getAllByText(/forty-two/);
   expect(text.length).toBe(1);
-})
+});
 
-test('User submits -42', () => {
-  render(<App/>)
+test("User submits -42", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-42'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-42" } });
   fireEvent.click(button);
   const text = screen.getAllByText(/negative forty-two/);
   expect(text.length).toBe(1);
-})
+});
 
-test('User submits 1999', () => {
-  render(<App/>)
+test("User submits 1999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '1999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "1999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/one thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /one thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
   const textUK = screen.getAllByText(/nineteen hundred and ninety-nine/);
   expect(textUK.length).toBe(1);
-})
+});
 
-test('User submits -1999', () => {
-  render(<App/>)
+test("User submits -1999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-1999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-1999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative one thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /negative one thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-  const textUK = screen.getAllByText(/negative nineteen hundred and ninety-nine/);
+  const textUK = screen.getAllByText(
+    /negative nineteen hundred and ninety-nine/
+  );
   expect(textUK.length).toBe(1);
-})
+});
 
-test('User submits 2001', () => {
-  render(<App/>)
+test("User submits 2001", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '2001'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "2001" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/two thousand and one/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -2001', () => {
-  render(<App/>)
+test("User submits -2001", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-2001'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-2001" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/negative two thousand and one/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 17999', () => {
-  render(<App/>)
+test("User submits 17999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '17999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "17999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/seventeen thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /seventeen thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -17999', () => {
-  render(<App/>)
+test("User submits -17999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-17999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-17999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative seventeen thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /negative seventeen thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 100001', () => {
-  render(<App/>)
+test("User submits 100001", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '100001'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "100001" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/one hundred thousand and one/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -100001', () => {
-  render(<App/>)
+test("User submits -100001", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-100001'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-100001" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/negative one hundred thousand and one/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 342251', () => {
-  render(<App/>)
+test("User submits 342251", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '342251'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "342251" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/three hundred and forty-two thousand two hundred and fifty-one/);
+  const textUS = screen.getAllByText(
+    /three hundred and forty-two thousand two hundred and fifty-one/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -342251', () => {
-  render(<App/>)
+test("User submits -342251", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-342251'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-342251" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative three hundred and forty-two thousand two hundred and fifty-one/);
+  const textUS = screen.getAllByText(
+    /negative three hundred and forty-two thousand two hundred and fifty-one/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-
-test('User submits 1300420', () => {
-  render(<App/>)
+test("User submits 1300420", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '1300420'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "1300420" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/one million three hundred thousand four hundred and twenty/);
+  const textUS = screen.getAllByText(
+    /one million three hundred thousand four hundred and twenty/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -1300420', () => {
-  render(<App/>)
+test("User submits -1300420", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-1300420'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-1300420" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative one million three hundred thousand four hundred and twenty/);
+  const textUS = screen.getAllByText(
+    /negative one million three hundred thousand four hundred and twenty/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 0', () => {
-  render(<App/>)
+test("User submits 0", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '0'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "0" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/zero/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits invalid input empty string', () => {
-  render(<App/>)
+test("User submits invalid input empty string", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: ''}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/Invalid input/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 999999999', () => {
-  render(<App/>)
+test("User submits 999999999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '999999999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "999999999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -999999999', () => {
-  render(<App/>)
+test("User submits -999999999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-999999999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-999999999" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 999999999.43', () => {
-  render(<App/>)
+test("User submits 999999999.43", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '999999999.43'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "999999999.43" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits -999999999.43', () => {
-  render(<App/>)
+test("User submits -999999999.43", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-999999999.43'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-999999999.43" } });
   fireEvent.click(button);
-  const textUS = screen.getAllByText(/negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/);
+  const textUS = screen.getAllByText(
+    /negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine/
+  );
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits invalid input 999999999.55', () => {
-  render(<App/>)
+test("User submits invalid input 999999999.55", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '999999999.55'}});
-  fireEvent.click(button);
-  const textUS = screen.getAllByText(/Invalid input/);
-  expect(textUS.length).toBe(1);
-})
-
-test('User submits invalid input -999999999.55', () => {
-  render(<App/>)
-  const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '-999999999.55'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "999999999.55" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/Invalid input/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits invalid input 12345678999', () => {
-  render(<App/>)
+test("User submits invalid input -999999999.55", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '12345678999'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "-999999999.55" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/Invalid input/);
   expect(textUS.length).toBe(1);
-})
+});
 
-test('User submits 1003', () => {
-  render(<App/>)
+test("User submits invalid input 12345678999", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '1003'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "12345678999" } });
+  fireEvent.click(button);
+  const textUS = screen.getAllByText(/Invalid input/);
+  expect(textUS.length).toBe(1);
+});
+
+test("User submits 1003", () => {
+  render(<App />);
+  const input = screen.getByPlaceholderText("Enter a number");
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "1003" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/one thousand and three/);
   expect(textUS.length).toBe(1);
   const textUK = screen.getAllByText(/ten hundred and three/);
   expect(textUK.length).toBe(1);
-})
+});
 
-test('User submits 1011', () => {
-  render(<App/>)
+test("User submits 1011", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '1011'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "1011" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/one thousand and eleven/);
   expect(textUS.length).toBe(1);
   const textUK = screen.getAllByText(/ten hundred and eleven/);
   expect(textUK.length).toBe(1);
-})
+});
 
-test('User submits 1230', () => {
-  render(<App/>)
+test("User submits 1230", () => {
+  render(<App />);
   const input = screen.getByPlaceholderText("Enter a number");
-  const button = screen.getByText('Convert');
-  fireEvent.change(input, {target: {value: '1230'}});
+  const button = screen.getByText("Convert");
+  fireEvent.change(input, { target: { value: "1230" } });
   fireEvent.click(button);
   const textUS = screen.getAllByText(/one thousand two hundred and thirty/);
   expect(textUS.length).toBe(1);
   const textUK = screen.getAllByText(/twelve hundred and thirty/);
   expect(textUK.length).toBe(1);
-})
+});
--- js-numerals/conversion/src/__tests__/conversion.spec.js
+++ js-numerals/conversion/src/__tests__/conversion.spec.js
@@ -1,114 +1,133 @@
-import {convertNumber, britishConversion} from '../conversion'
+import { convertNumber, britishConversion } from "../conversion";
 
-test('7 === seven', () => {
-  expect(convertNumber(7)).toBe('seven');
-  expect(britishConversion(7)).toBe('seven');
-})
+test("7 === seven", () => {
+  expect(convertNumber(7)).toBe("seven");
+  expect(britishConversion(7)).toBe("seven");
+});
 
-test('-7 === negative seven', () => {
-  expect(convertNumber(-7)).toBe('negative seven');
-  expect(britishConversion(-7)).toBe('negative seven');
-})
+test("-7 === negative seven", () => {
+  expect(convertNumber(-7)).toBe("negative seven");
+  expect(britishConversion(-7)).toBe("negative seven");
+});
 
-test('42 === forty-two', () => {
-  expect(convertNumber(42)).toBe('forty-two');
-  expect(britishConversion(42)).toBe('forty-two');
-})
+test("42 === forty-two", () => {
+  expect(convertNumber(42)).toBe("forty-two");
+  expect(britishConversion(42)).toBe("forty-two");
+});
 
-test('-42 === negative forty-two', () => {
-  expect(convertNumber(-42)).toBe('negative forty-two');
-  expect(britishConversion(-42)).toBe('negative forty-two');
-})
+test("-42 === negative forty-two", () => {
+  expect(convertNumber(-42)).toBe("negative forty-two");
+  expect(britishConversion(-42)).toBe("negative forty-two");
+});
 
-test('1999 === one thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(1999)).toBe('one thousand nine hundred and ninety-nine');
-})
+test("1999 === one thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(1999)).toBe("one thousand nine hundred and ninety-nine");
+});
 
-test('-1999 === negativeone thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(-1999)).toBe('negative one thousand nine hundred and ninety-nine');
-})
+test("-1999 === negativeone thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(-1999)).toBe(
+    "negative one thousand nine hundred and ninety-nine"
+  );
+});
 
-test('1999 === nineteen hundred and ninety-nine', () => {
-  expect(britishConversion(1999)).toBe('nineteen hundred and ninety-nine');
-})
+test("1999 === nineteen hundred and ninety-nine", () => {
+  expect(britishConversion(1999)).toBe("nineteen hundred and ninety-nine");
+});
 
-test('-1999 === negative nineteen hundred and ninety-nine', () => {
-  expect(britishConversion(-1999)).toBe('negative nineteen hundred and ninety-nine');
-})
+test("-1999 === negative nineteen hundred and ninety-nine", () => {
+  expect(britishConversion(-1999)).toBe(
+    "negative nineteen hundred and ninety-nine"
+  );
+});
 
-test('1003 === ten hundred and three', () => {
-  expect(britishConversion(1003)).toBe('ten hundred and three');
-})
+test("1003 === ten hundred and three", () => {
+  expect(britishConversion(1003)).toBe("ten hundred and three");
+});
 
-test('1003 === one thousand and three', () => {
-  expect(convertNumber(1003)).toBe('one thousand and three');
-})
+test("1003 === one thousand and three", () => {
+  expect(convertNumber(1003)).toBe("one thousand and three");
+});
 
-test('1011 === one thousand and eleven', () => {
-  expect(convertNumber(1011)).toBe('one thousand and eleven');
-})
+test("1011 === one thousand and eleven", () => {
+  expect(convertNumber(1011)).toBe("one thousand and eleven");
+});
 
-test('1011 === ten hundred and eleven', () => {
-  expect(britishConversion(1011)).toBe('ten hundred and eleven');
-})
+test("1011 === ten hundred and eleven", () => {
+  expect(britishConversion(1011)).toBe("ten hundred and eleven");
+});
 
-test('1230 === twelve hundred and thirty', () => {
-  expect(britishConversion(1230)).toBe('twelve hundred and thirty');
-})
+test("1230 === twelve hundred and thirty", () => {
+  expect(britishConversion(1230)).toBe("twelve hundred and thirty");
+});
 
-test('1230 === one thousand two hundred and thirty', () => {
-  expect(convertNumber(1230)).toBe('one thousand two hundred and thirty');
-})
+test("1230 === one thousand two hundred and thirty", () => {
+  expect(convertNumber(1230)).toBe("one thousand two hundred and thirty");
+});
 
-test('2001 === two thousand and one', () => {
-  expect(convertNumber(2001)).toBe('two thousand and one');
-})
+test("2001 === two thousand and one", () => {
+  expect(convertNumber(2001)).toBe("two thousand and one");
+});
 
-test('-2001 === negative two thousand and one', () => {
-  expect(convertNumber(-2001)).toBe('negative two thousand and one');
-})
+test("-2001 === negative two thousand and one", () => {
+  expect(convertNumber(-2001)).toBe("negative two thousand and one");
+});
 
+test("17999 === seventeen thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(17999)).toBe(
+    "seventeen thousand nine hundred and ninety-nine"
+  );
+});
 
-test('17999 === seventeen thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(17999)).toBe('seventeen thousand nine hundred and ninety-nine');
-})
+test("-17999 === negative seventeen thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(-17999)).toBe(
+    "negative seventeen thousand nine hundred and ninety-nine"
+  );
+});
 
-test('-17999 === negative seventeen thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(-17999)).toBe('negative seventeen thousand nine hundred and ninety-nine');
-})
+test("100001 === one hundred thousand and one", () => {
+  expect(convertNumber(100001)).toBe("one hundred thousand and one");
+});
 
-test('100001 === one hundred thousand and one', () => {
-  expect(convertNumber(100001)).toBe('one hundred thousand and one');
-})
+test("-100001 === negative one hundred thousand and one", () => {
+  expect(convertNumber(-100001)).toBe("negative one hundred thousand and one");
+});
 
-test('-100001 === negative one hundred thousand and one', () => {
-  expect(convertNumber(-100001)).toBe('negative one hundred thousand and one');
-})
+test("342251  === three hundred and forty-two thousand two hundred and fifty-one", () => {
+  expect(convertNumber(342251)).toBe(
+    "three hundred and forty-two thousand two hundred and fifty-one"
+  );
+});
 
-test('342251  === three hundred and forty-two thousand two hundred and fifty-one', () => {
-  expect(convertNumber(342251)).toBe('three hundred and forty-two thousand two hundred and fifty-one');
-})
+test("-342251  === negative three hundred and forty-two thousand two hundred and fifty-one", () => {
+  expect(convertNumber(-342251)).toBe(
+    "negative three hundred and forty-two thousand two hundred and fifty-one"
+  );
+});
 
-test('-342251  === negative three hundred and forty-two thousand two hundred and fifty-one', () => {
-  expect(convertNumber(-342251)).toBe('negative three hundred and forty-two thousand two hundred and fifty-one');
-})
+test("1300420 === one million three hundred thousand four hundred and twenty", () => {
+  expect(convertNumber(1300420)).toBe(
+    "one million three hundred thousand four hundred and twenty"
+  );
+});
 
-test('1300420 === one million three hundred thousand four hundred and twenty', () => {
-  expect(convertNumber(1300420)).toBe('one million three hundred thousand four hundred and twenty');
-})
+test("-1300420 === negative one million three hundred thousand four hundred and twenty", () => {
+  expect(convertNumber(-1300420)).toBe(
+    "negative one million three hundred thousand four hundred and twenty"
+  );
+});
 
-test('-1300420 === negative one million three hundred thousand four hundred and twenty', () => {
-  expect(convertNumber(-1300420)).toBe('negative one million three hundred thousand four hundred and twenty');
-})
+test("0 === zero", () => {
+  expect(convertNumber(0)).toBe("zero");
+});
 
-test('0 === zero', () => {
-  expect(convertNumber(0)).toBe('zero');
-})
+test("999999999 === nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(999999999)).toBe(
+    "nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine"
+  );
+});
 
-test('999999999 === nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(999999999)).toBe('nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine');
-})
-
-test('-999999999 === negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine', () => {
-  expect(convertNumber(-999999999)).toBe('negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine');
-})
+test("-999999999 === negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine", () => {
+  expect(convertNumber(-999999999)).toBe(
+    "negative nine hundred and ninety-nine million nine hundred and ninety-nine thousand nine hundred and ninety-nine"
+  );
+});
