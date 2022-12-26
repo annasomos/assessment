5fe65117bf39567ed87bac66e8589f2fd8ccc908
annasomos <annasomoss@gmail.com>
2022-12-26 19:33:46 +0100

    Feature: Pagination function to handle previous button click

--- js-users/users-list-assessment/src/components/UserPagination.tsx
+++ js-users/users-list-assessment/src/components/UserPagination.tsx
@@ -1,18 +1,27 @@
 import { UserModel } from "../model/UserModel";
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
 
 interface PaginationProps {
   users: UserModel[];
 }
 
-const UserPagination: React.FC<PaginationProps> = ({ users }: PaginationProps) => {
-  
+const UserPagination: React.FC<PaginationProps> = ({
+  users,
+}: PaginationProps) => {
+
   const shownUsersPerPage: number = 10;
+
+  const numberOfPages: number = Math.ceil(users.length / shownUsersPerPage);
+
+  const [pageNumbers, setPageNumbers] = useState<number[]>();
+
+  const [minPagesShown, setMinPagesShown] = useState<number>(0);
+
+  const [maxPagesShown, setMaxPagesShown] = useState<number>(3);
 
   const [currentPageNumber, setCurrentPageNumber] = useState<number>(1);
 
   const [currentPageUsers, setCurrentPageUsers] = useState<UserModel[]>();
-
 
   useEffect(() => {
     const startNumber = (currentPageNumber - 1) * shownUsersPerPage;
@@ -21,10 +30,15 @@ const UserPagination: React.FC<PaginationProps> = ({ users }: PaginationProps) =
     setCurrentPageUsers(usersPerCurrentPage);
   }, [currentPageNumber, users]);
 
-  return (
-    <>
-    </>
-  )
+  function handlePreviousButtonClick() {
+    setCurrentPageNumber((currentPageNum) => currentPageNum - 1);
+    setMinPagesShown((currentNumber) => currentNumber - 1);
+    setMaxPagesShown((currentNumber) => currentNumber - 1);
+  }
+
+
+
+  return <></>;
 };
 
 export default UserPagination;
