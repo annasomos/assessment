04b779d89a51f9523a02bc66b5861cb5616563dc
annasomos <annasomoss@gmail.com>
2022-12-29 17:08:45 +0100

    Refactor: Error handling in user edit component

--- js-users/users-list-assessment/src/components/EditUser.tsx
+++ js-users/users-list-assessment/src/components/EditUser.tsx
@@ -6,13 +6,15 @@ import { UserModel } from "../model/UserModel";
 export const EditUser = () => {
   const [user, setUser] = useState<UserModel>();
 
-  const [isUserFound, setIsUserFound] = useState<boolean>(true);
+  const [isUserFound, setIsUserFound] = useState<boolean>(false);
 
-  const [errorMessage, setErrorMessage] = useState<string>();
+  const [errorMessage, setErrorMessage] = useState<string>("User Not Found");
 
   const navigateTo = useNavigate();
 
   const params = useParams();
+
+  const[userId, setUserId] = useState<string>(params.id as string);
 
   type updatedUser = {
     first_name: string;
@@ -22,10 +24,10 @@ export const EditUser = () => {
   useEffect(() => {
     const getUserById = async () => {
       try {
-        return await api.getUserById(params.id as string);
+        setIsUserFound(true);
+        return await api.getUserById(userId);
       } catch (error) {
         setIsUserFound(false);
-        setErrorMessage("User Not Found");
       }
     };
 
@@ -40,35 +42,41 @@ export const EditUser = () => {
       first_name: event.target["first_name"].value,
       last_name: event.target["last_name"].value,
     };
-    await api.updateUserById(params.id as string, updatedUser);
+    await api.updateUserById(userId, updatedUser);
     navigateTo("../", { replace: true });
   }
 
   return (
-    <div className="edit-form">
-      <form onSubmit={(event) => updateUser(event)}>
-        <div className="edit-row">
-          <label>First Name:</label>
-          <input
-            required={true}
-            type="text"
-            placeholder="First Name"
-            name="first_name"
-            defaultValue={user?.first_name}
-          />
+    <div className="edit-container">
+      {isUserFound ? (
+        <div className="edit-form">
+          <form onSubmit={(event) => updateUser(event)}>
+            <div className="edit-row">
+              <label>First Name:</label>
+              <input
+                required={true}
+                type="text"
+                placeholder="First Name"
+                name="first_name"
+                defaultValue={user?.first_name}
+              />
+            </div>
+            <div className="edit-row">
+              <label>Last Name:</label>
+              <input
+                required={true}
+                type="text"
+                placeholder="Last Name"
+                name="last_name"
+                defaultValue={user?.last_name}
+              />
+            </div>
+            <button type="submit">Submit</button>
+          </form>
         </div>
-        <div className="edit-row">
-          <label>Last Name:</label>
-          <input
-            required={true}
-            type="text"
-            placeholder="Last Name"
-            name="last_name"
-            defaultValue={user?.last_name}
-          />
-        </div>
-        <button type="submit">Submit</button>
-      </form>
+      ) : (
+        <div className="error-container">{errorMessage}</div>
+      )}
     </div>
   );
 };
