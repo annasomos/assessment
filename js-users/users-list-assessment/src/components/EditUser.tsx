ef9750b910d997ea49c7ad308c9838640f1c9a5a
annasomos <annasomoss@gmail.com>
2022-12-29 15:22:35 +0100

    Refactor: Add edit user route to router

--- js-users/users-list-assessment/src/components/EditUser.tsx
+++ js-users/users-list-assessment/src/components/EditUser.tsx
@@ -1,17 +1,66 @@
-import {useState} from "react";
+import {useState, useEffect} from "react";
+import {useNavigate, useParams} from "react-router-dom";
+import { api } from "../api/api";
+import { UserModel } from "../model/UserModel";
 
 
 export const EditUser = () => {
 
+const [user, setUser] = useState<UserModel>();
+
 const [isUserFound, setIsUserFound] = useState<boolean>(true);
+
+const [errorMessage, setErrorMessage] = useState<string>()
+
+const navigateTo = useNavigate();
+
+const params = useParams();
 
 type updatedUser = {
   first_name: string;
   last_name: string;
 }
 
+useEffect(() => {
+  const getUserById = async () => {
+    try {
+      return await api.getUserById(params.id as string);
+    } catch (error) {
+      setIsUserFound(false)
+      setErrorMessage("User Not Found")
+    } 
+  };
+
+  getUserById().then((user) => {
+    setUser(user);
+  });
+}, [params.id]);
+
   return (
-    <div>EditUser</div>
+    <div className="edit-form">
+      <form>
+      <label>
+              First Name:
+      </label>
+            <input
+              required={true}
+              type="text"
+              placeholder="First Name"
+              name="first_name"
+              defaultValue={user?.first_name}
+            />
+            <label>
+              Last Name:
+      </label>
+            <input
+              required={true}
+              type="text"
+              placeholder="Last Name"
+              name="last_name"
+              defaultValue={user?.last_name}
+            />
+      </form>
+    </div>
   )
 }
 
--- js-users/users-list-assessment/src/controller/Router.tsx
+++ js-users/users-list-assessment/src/controller/Router.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route} from 'react-router-dom';
 import App from "../App";
+import EditUser from '../components/EditUser';
 import UserList from "../components/UserList"
 
 
@@ -9,6 +10,7 @@ const Router = () => {
     <Routes>
       <Route element={<App />}>
       <Route index element={<UserList />} />
+      <Route path="/edit/:id" element={<EditUser/>} />
       </Route>
     </Routes>
 );
