681e17e189ed979683c0da9de77a0057bd9e5f1c
annasomos <annasomoss@gmail.com>
2023-01-01 21:32:18 +0100

    Feature: Add loading spinner while loading

--- js-users/users-list-assessment/src/components/UserList.tsx
+++ js-users/users-list-assessment/src/components/UserList.tsx
@@ -1,15 +1,19 @@
-import {useState, useEffect} from "react";
-import {UserModel} from "../model/UserModel";
+import { useState, useEffect } from "react";
+import { UserModel } from "../model/UserModel";
 import UserPagination from "./UserPagination";
-import {api} from "../api/api";
+import { api } from "../api/api";
+import { MDBSpinner } from "mdb-react-ui-kit";
 
 const UserList = () => {
-
   const [allUsers, setAllUsers] = useState<UserModel[]>([]);
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   async function getUsers() {
+    setIsLoading(true);
     const users = await api.getAllUsers();
     setAllUsers(users);
+    setIsLoading(false);
   }
 
   useEffect(() => {
@@ -17,9 +21,15 @@ const UserList = () => {
   }, []);
 
   return (
-    <div className="cards">
+    <>
+      {isLoading ? (
+        <MDBSpinner role="status">
+          <span className="visually-hidden">Loading...</span>
+        </MDBSpinner>
+      ) : (
         <UserPagination users={allUsers} />
-    </div>
+      )}
+    </>
   );
 };
 
