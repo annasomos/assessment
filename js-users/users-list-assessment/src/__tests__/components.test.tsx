9409f25107d5802cfbb58c602a49ffc068a3f2ab
annasomos <annasomoss@gmail.com>
2023-01-03 10:30:14 +0100

    Refactor: Add mock data to separate file

--- js-users/users-list-assessment/src/__tests__/components.test.tsx
+++ js-users/users-list-assessment/src/__tests__/components.test.tsx
@@ -1,10 +1,10 @@
 import UserPagination from "../components/UserPagination";
 import { render, screen, cleanup } from "@testing-library/react";
-import { BrowserRouter, useNavigate } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import User from "../components/User";
 import UserForm from "../components/UserForm";
 import AddNewUser from "../components/AddNewUser";
-import { api } from "../api/api";
+import { mockResponse } from "../util/testData";
 
 beforeEach(() => {
   cleanup();
@@ -14,73 +14,7 @@ afterEach(() => {
   cleanup();
 });
 
-const mockResponse = [
-  {
-    id: 723,
-    last_name: "Luna",
-    first_name: "Lovegood",
-    status: "active",
-    created_at: "2022-12-07T07:22:14.839Z",
-    updated_at: "2023-01-02T17:32:29.893Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/723.json",
-  },
-  {
-    id: 727,
-    last_name: "Harry",
-    first_name: "Potter",
-    status: "locked",
-    created_at: "2022-12-07T19:54:08.096Z",
-    updated_at: "2023-01-02T09:07:58.865Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/727.json",
-  },
-  {
-    id: 838,
-    last_name: "Ronald",
-    first_name: "Weasley",
-    status: "locked",
-    created_at: "2022-12-12T01:01:56.417Z",
-    updated_at: "2022-12-27T22:27:04.762Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/838.json",
-  },
-  {
-    id: 820,
-    last_name: "Hermione",
-    first_name: "Granger",
-    status: "locked",
-    created_at: "2022-12-12T00:26:59.746Z",
-    updated_at: "2023-01-02T09:42:36.174Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/820.json",
-  },
-  {
-    id: 607,
-    last_name: "Neville",
-    first_name: "Longbottom",
-    status: "active",
-    created_at: "2022-12-02T15:55:58.655Z",
-    updated_at: "2022-12-29T17:36:06.737Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/607.json",
-  },
-  {
-    id: 725,
-    last_name: "Sirius",
-    first_name: "Black",
-    status: "active",
-    created_at: "2022-12-07T08:17:07.918Z",
-    updated_at: "2022-12-27T22:27:10.726Z",
-    url: "https://assessment-users-backend.herokuapp.com/users/725.json",
-  },
-];
-
-const handleUpdateOnSubmit = async (event: any, userId: string) => {
-  const navigateTo = useNavigate();
-  event.preventDefault();
-  const updatedUser = {
-    first_name: event.target["first_name"].value,
-    last_name: event.target["last_name"].value,
-  };
-  await api.updateUserById(userId, updatedUser);
-  navigateTo("../");
-};
+const handleUpdateOnSubmit = jest.fn();
 
 test("List all users, get 6 user cards on screen", () => {
   render(
@@ -247,12 +181,7 @@ test("Render AddNewUser component", () => {
 test("Render UserForm component of User 725", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[5]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[5].id}`)
-        }
-      />
+      <UserForm user={mockResponse[5]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Sirius/);
@@ -264,12 +193,7 @@ test("Render UserForm component of User 725", () => {
 test("Render UserForm component of User 723", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[0]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[0].id}`)
-        }
-      />
+      <UserForm user={mockResponse[0]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Luna/);
@@ -281,12 +205,7 @@ test("Render UserForm component of User 723", () => {
 test("Render UserForm component of User 727", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[1]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[1].id}`)
-        }
-      />
+      <UserForm user={mockResponse[1]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Harry/);
@@ -298,12 +217,7 @@ test("Render UserForm component of User 727", () => {
 test("Render UserForm component of User 838", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[2]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[2].id}`)
-        }
-      />
+      <UserForm user={mockResponse[2]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Ronald/);
@@ -315,12 +229,7 @@ test("Render UserForm component of User 838", () => {
 test("Render UserForm component of User 820", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[3]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[3].id}`)
-        }
-      />
+      <UserForm user={mockResponse[3]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Hermione/);
@@ -332,12 +241,7 @@ test("Render UserForm component of User 820", () => {
 test("Render UserForm component of User 607", () => {
   render(
     <BrowserRouter>
-      <UserForm
-        user={mockResponse[4]}
-        handleOnSubmit={(e: any) =>
-          handleUpdateOnSubmit(e, `${mockResponse[4].id}`)
-        }
-      />
+      <UserForm user={mockResponse[4]} handleOnSubmit={handleUpdateOnSubmit} />
     </BrowserRouter>
   );
   const first_name = screen.getAllByDisplayValue(/Neville/);
