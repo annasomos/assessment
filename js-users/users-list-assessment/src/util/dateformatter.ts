3ccdc0de853c1aa809fa93fc68d16cf61c843fb8
annasomos <annasomoss@gmail.com>
2023-01-12 11:52:30 +0100

    Refactor: remove magic numbers from dateformatter

--- js-users/users-list-assessment/src/util/dateformatter.ts
+++ js-users/users-list-assessment/src/util/dateformatter.ts
@@ -1,8 +1,7 @@
-const dateStart: number = 0;
-const dateEnd: number = 10;
-const timeStart: number = 11;
-const timeEnd: number = 16;
-
 export function formatDate(date: string) {
-  return `${date.slice(dateStart, dateEnd)} ${date.slice(timeStart, timeEnd)}`;
+  const splitDate: string[] = date.split("T");
+  const dateFormatted: string = splitDate[0];
+  const splitTime: string[] = splitDate[1].split(":");
+  const timeFormatted: string = `${splitTime[0]}:${splitTime[1]}`;
+  return `${dateFormatted} ${timeFormatted}`;
 }
